c75dffc1d2ac54ba77133033986992e846e6dbbd
Mert İlhan <mertilhan1018@gmail.com>
2021-03-22 17:12:35 +0300

    geo manager eklendi

---
+++ ShippingCalculator.BusinessLogicLayer/Concrete/GeoManager.cs
@@ -0,0 +1,16 @@
+﻿using ShippingCalculator.CommonLayer.Logger.Concrete;
+using ShippingCalculator.DataAccessLayer.PostgreSQL;
+using Npgsql;
+namespace ShippingCalculator.BusinessLogicLayer.Concrete
+{
+    public class GeoManager
+    {
+        private readonly Logger logger;
+        private Database database;
+        private NpgsqlDataReader reader;
+        public GeoManager(Logger _logger)
+        {
+            logger = _logger;
+        }
+    }
+}
