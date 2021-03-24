a98a0ed7350f14c37b158b4f6a3090b8bffbba52
Mert İlhan <mertilhan1018@gmail.com>
2021-03-24 17:17:36 +0300

    Admin Panel İçin AdminPanel Area'sı Oluşturuldu

---
+++ ShippingCalculator.WebUI/Areas/AdminPanel/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShippingCalculator.WebUI.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
