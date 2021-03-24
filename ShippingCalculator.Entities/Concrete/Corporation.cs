96fbf466a1583571aa58e6928c5af71c43035023
Mert İlhan <mertilhan1018@gmail.com>
2021-03-24 17:16:36 +0300

    Kurumsal Müşteri İçin Corporation Nesnesi Oluşturuldu.

---
+++ ShippingCalculator.Entities/Concrete/Corporation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingCalculator.Entities.Concrete
+{
+    public class Corporation
+    {
+        public int Id { get; set; }
+        public string Password { get; set; }
+        public decimal Discount { get; set; }
+    }
+}
