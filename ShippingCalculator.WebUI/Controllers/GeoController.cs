2f142b3748cbf375c3c8f7a4caaac54b64025461
Mert İlhan <mertilhan1018@gmail.com>
2021-03-24 17:17:06 +0300

    GeoController Oluşturuldu.

---
+++ ShippingCalculator.WebUI/Controllers/GeoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShippingCalculator.WebUI.Controllers
+{
+    public class GeoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
