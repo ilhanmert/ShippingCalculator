9f8e7e0b26eef19b2f40b0ec43c141f8ae02ef68
Mert İlhan <mertilhan1018@gmail.com>
2021-03-26 16:35:38 +0300

    ShippingCompanyManager Oluşturuldu.

---
+++ ShippingCalculator.BusinessLogicLayer/Concrete/ShippingCompanyManager.cs
@@ -0,0 +1,18 @@
+﻿using Npgsql;
+using ShippingCalculator.CommonLayer.Logger.Concrete;
+using ShippingCalculator.DataAccessLayer.PostgreSQL;
+
+namespace ShippingCalculator.BusinessLogicLayer.Concrete
+{
+    public class ShippingCompanyManager
+    {
+        private readonly Logger logger;
+        private Database database;
+        private NpgsqlDataReader reader;
+        public ShippingCompanyManager(Logger _logger)
+        {
+            logger = _logger;
+        }
+
+    }
+}
--- ShippingCalculator.Entities/Concrete/ShippingCompany.cs
+++ ShippingCalculator.Entities/Concrete/ShippingCompany.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ShippingCalculator.Entities.Concrete
+﻿namespace ShippingCalculator.Entities.Concrete
 {
     public class ShippingCompany
     {
