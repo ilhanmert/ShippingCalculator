78d8d37d8523de3864de92a8f5bb060187e24a43
Mert İlhan <mertilhan1018@gmail.com>
2021-03-26 16:35:03 +0300

    Kurumsal Müşteri İçin Kurum Adı ve Kurum Numarası Property'leri Eklendi.

--- ShippingCalculator.Entities/Concrete/Corporation.cs
+++ ShippingCalculator.Entities/Concrete/Corporation.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ShippingCalculator.Entities.Concrete
+﻿namespace ShippingCalculator.Entities.Concrete
 {
     public class Corporation
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public int Number { get; set; }
         public string Password { get; set; }
         public decimal Discount { get; set; }
     }
