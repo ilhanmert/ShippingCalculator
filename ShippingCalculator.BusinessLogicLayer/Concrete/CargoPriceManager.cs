0bc78cc129f845152754600c5bdbf60844a45f45
Mert İlhan <mertilhan1018@gmail.com>
2021-04-02 17:09:43 +0300

    CargoPriceManager eklendi.

---
+++ ShippingCalculator.BusinessLogicLayer/Concrete/CargoPriceManager.cs
@@ -0,0 +1,49 @@
+﻿using ShippingCalculator.CommonLayer.Logger.Concrete;
+using System;
+
+namespace ShippingCalculator.BusinessLogicLayer.Concrete
+{
+    class CargoPriceManager
+    {
+        private readonly Logger logger;
+        public CargoPriceManager(Logger _logger)
+        {
+            logger = _logger;
+        }
+        public double LocalPriceCalculator(int desi)
+        {
+            double result;
+            result = Math.Pow(1.082, (desi - 1));
+            result = (result * 1.45) * 11.43;
+            return result;
+        }
+        public double ClosePriceCalculator(int desi)
+        {
+            double result;
+            result = Math.Pow(1.082, (desi - 1));
+            result = (result * 2.15) * 11.43;
+            return result;
+        }
+        public double ShortPriceCalculator(int desi)
+        {
+            double result;
+            result = Math.Pow(1.082, (desi - 1));
+            result = (result * 2.33) * 11.43;
+            return result;
+        }
+        public double MidlinePriceCalculator(int desi)
+        {
+            double result;
+            result = Math.Pow(1.082, (desi - 1));
+            result = (result * 2.56) * 11.43;
+            return result;
+        }
+        public double LongPriceCalculator(int desi)
+        {
+            double result;
+            result = Math.Pow(1.082, (desi - 1));
+            result = (result * 2.82) * 11.43;
+            return result;
+        }
+    }
+}
