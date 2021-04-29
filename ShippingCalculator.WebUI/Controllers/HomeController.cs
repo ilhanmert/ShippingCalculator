39afc6226fff12e091c88208421c2c8af97e80d3
Mert İlhan <mertilhan1018@gmail.com>
2021-04-29 15:01:22 +0300

    PackageCalculate ve DocumentCalculate Eklendi.

--- ShippingCalculator.WebUI/Controllers/HomeController.cs
+++ ShippingCalculator.WebUI/Controllers/HomeController.cs
@@ -2,7 +2,7 @@
 using Microsoft.Extensions.Logging;
 using ShippingCalculator.BusinessLogicLayer.Concrete;
 using ShippingCalculator.Entities.Concrete;
-using System;
+using System.Collections.Generic;
 
 namespace ShippingCalculator.WebUI.Controllers
 {
@@ -10,29 +10,88 @@ namespace ShippingCalculator.WebUI.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly CargoPriceManager _cargoPriceManager;
-        public HomeController(ILogger<HomeController> logger, CargoPriceManager cargoPriceManager)
+        private readonly ShippingCompanyManager _shippingCompanyManager;
+        public HomeController(ILogger<HomeController> logger, CargoPriceManager cargoPriceManager, ShippingCompanyManager shippingCompanyManager)
         {
             _cargoPriceManager = cargoPriceManager;
             _logger = logger;
+            _shippingCompanyManager = shippingCompanyManager;
         }
         public IActionResult Index()
         {
             return View();
         }
-
         public IActionResult Privacy()
         {
             return View();
         }
-        public IActionResult Main()
-        {
-            return View();
-        }
         [HttpPost]
-        public IActionResult Desi(Package package)
+        public decimal PackageCalculate(int distance, Package package)
         {
             int desi = _cargoPriceManager.DesiCalculator(package);
-            return View(desi);
+            decimal price;
+            if (distance <= 1)
+            {
+                price = _cargoPriceManager.LocalPriceCalculator(desi);
+            }
+            else if (distance <= 200 && distance > 1)
+            {
+                price = _cargoPriceManager.ClosePriceCalculator(desi);
+            }
+            else if (distance <= 600 && distance > 200)
+            {
+                price = _cargoPriceManager.ShortPriceCalculator(desi);
+            }
+            else if (distance <= 1000 && distance > 600)
+            {
+                price = _cargoPriceManager.MidlinePriceCalculator(desi);
+            }
+            else
+            {
+                price = _cargoPriceManager.LongPriceCalculator(desi);
+            }
+            return price;
+        }
+        [HttpPost]
+        public decimal DocumentCalculate(int distance)
+        {
+            int desi = 1;
+            decimal price;
+            if (distance <= 1)
+            {
+                price = _cargoPriceManager.LocalPriceCalculator(desi);
+            }
+            else if (distance > 1 && distance <= 200)
+            {
+                price = _cargoPriceManager.ClosePriceCalculator(desi);
+            }
+            else if (distance > 200 && distance <= 600)
+            {
+                price = _cargoPriceManager.ShortPriceCalculator(desi);
+            }
+            else if (distance > 600 && distance <= 1000)
+            {
+                price = _cargoPriceManager.MidlinePriceCalculator(desi);
+            }
+            else
+            {
+                price = _cargoPriceManager.LongPriceCalculator(desi);
+            }
+            return price;
+        }
+        public PartialViewResult CargoCompaniesList()
+        {
+            List<ShippingCompany> cargoCompanies = _shippingCompanyManager.GetShippingCompaniesJsonList();
+            if (cargoCompanies != null && cargoCompanies.Count > 0)
+            {
+                return PartialView("CargoCompaniesList", cargoCompanies);
+            }
+            return PartialView("CargoCompaniesList", null);
+        }
+        [HttpPost]
+        public PartialViewResult GenerateNewPackage(List<Package> packages)
+        {
+            return PartialView("GenerateNewPackage", packages);
         }
     }
 }
