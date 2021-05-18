7a8ca5b8140291f497dcb10e4be1ea4c9cbaa2b4
Mert İlhan <mertilhan1018@gmail.com>
2021-05-18 17:31:45 +0300

    Mesafelere Göre Katsayılar ve Price Değişkeni ShippingCompany Nesnesine Eklendi.

--- ShippingCalculator.Entities/Concrete/ShippingCompany.cs
+++ ShippingCalculator.Entities/Concrete/ShippingCompany.cs
@@ -4,5 +4,11 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public decimal LocalFactor { get; set; }
+        public decimal CloseFactor { get; set; }
+        public decimal ShortFactor { get; set; }
+        public decimal MiddleFactor { get; set; }
+        public decimal LongFactor { get; set; }
+        public decimal Price { get; set; }
     }
 }
